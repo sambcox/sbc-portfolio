850eb97eb9af84a879ebfe1ceabc5e2e5efbf924
Samuel Cox <samc1253@gmail.com>
2023-06-20 11:17:57 -0600

    Update apostrophes

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="about-me-bg mb-4 p-6 shadow sm:rounded-lg">
             <p className="text-lg text-gray-100">
-              As a dedicated software engineer, I specialize in back end development. This website was built as an exploration of a new technology and language for me, Next.JS and JavaScript. As someone for whom constant learning is crucial, this was a fantastic experience. To learn more about me and all of the other projects I've completed, use the navigation bar above!
+              As a dedicated software engineer, I specialize in back end development. This website was built as an exploration of a new technology and language for me, Next.JS and JavaScript. As someone for whom constant learning is crucial, this was a fantastic experience. To learn more about me and all of the other projects I&apos;ve completed, use the navigation bar above!
             </p>
             <div className="image-container py-4 px-2">
               <Slideshow />
